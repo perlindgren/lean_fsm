3377edb69932a0d6184a87c40a33423a2ec6efd2
Per Lindgren <per.lindgren@ltu.se>
2025-04-28 16:56:17 +0200

    liveness

--- LeanFsm/Basic.lean
+++ LeanFsm/Basic.lean
@@ -23,12 +23,11 @@ match s, ta, tb with
 #reduce (next s0 false false)
 #reduce (next s0 false true)
 
-theorem s0_sure : ∀ tb, next s0 false tb = s1:=
+theorem s0_quant : ∀ tb, next s0 false tb = s1:=
   by
     simp [next]
 
-
-
+-- Gray encoding
 def es (s:S) : BitVec 2 :=
 match s with
 | s0 => 0b00
@@ -91,3 +90,57 @@ match s with
   let b_bv := eo b
   (a_bv[1], a_bv[0], b_bv[1], b_bv[0])
   )
+
+theorem safety : ∀ s,
+  match out s with
+  | (green, green) => false
+  | (_    , _    ) => true
+  := by
+    intro s
+    cases s <;> simp [out]
+
+def trans (s: S) (i: List (Bool × Bool)) : S :=
+  match i with
+  | [] => s
+  | (ta, tb) :: il => trans (next s ta tb) il
+
+
+-- proof that for any state there is at least one path
+-- to any other state, thus liveness holds (its never "stuck")
+theorem liveness: ∀ (s s': S), ∃ (i: List (Bool × Bool)),
+  trans s i = s'
+  := by
+    intro s s'
+    cases s <;> cases s'
+    . -- s0 -> s0
+      exists []
+    . -- s0 -> s1
+      exists [(false, default)]
+    . -- s0 -> s1 -> s2
+      exists [(false, default), (default, default)]
+    . -- s0 -> s1 -> s2 -> s3
+      exists [(false, default), (default, default), (default, false)]
+    . -- s1 -> s2 -> s3 -> s0
+      exists [(default, default), (default, false), (default, default)]
+    . -- s1 -> s1
+      exists []
+    . -- s1 -> s2
+      exists [(default, default)]
+    . -- s1 -> s2 -> s3
+      exists [(default, default), (default, false)]
+    . -- s2 -> s3 -> s0
+      exists [(default, false), (default, default)]
+    . -- s2 -> s3 -> s0 -> s1
+      exists [(default, false), (default, default), (false, default)]
+    . -- s2 -> s2
+      exists []
+    . -- s2 -> s3
+      exists [(default, default)]
+    . -- s3 -> s0
+      exists [(default, default)]
+    . -- s3 -> s0 -> s1
+      exists [(default, default), (false, default)]
+    . -- s3 -> s0 -> s1 -> s2
+      exists [(default, default), (false, default), (default, default)]
+    . -- s3 -> s3
+      exists []
